0c6f0b070335f6d11f2c08fbb42da5581ad3570d
jeff <bbmyth@gmail.com>
2018-06-22 03:16:49 +0800

    增加减法函数

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ def add(x, y):
 def multiple(x, y):
     return x * y
 
+def sub(x, y):
+    retur x - y
+
 def main():
     add(1024, 1024)
     multiple(8, 8)
+    sub(1024, 24)
 
 if __name__ == '__main__':
     main()
