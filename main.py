0f99abf2ce5f23b56b6bc0bed9dc2b63bfd87786
jeff <bbmyth@gmail.com>
2018-06-22 04:06:24 +0800

    增加除法

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def multiple(x, y):
     return x * y
 
 def sub(x, y):
-    retur x - y
+    return x - y
+
+def div(x, y):
+    return x / y
 
 def main():
     add(1024, 1024)
     multiple(8, 8)
     sub(1024, 24)
+    div(1024, 256)
 
 if __name__ == '__main__':
     main()
